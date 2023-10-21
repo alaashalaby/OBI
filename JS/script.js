d05d1b4352fecaff2186fe80345824a315d0a0ed
Alaa Shalaby <alaashalabi011@gmail.com>
2023-10-22 01:13:02 +0200

    Edit

--- JS/script.js
+++ JS/script.js
@@ -70,6 +70,35 @@ window.onscroll = calcScrollValue;
 window.onload = calcScrollValue;
 
 /* ======================================= */
+/* map */
+
+let map;
+async function initMap() {
+  const position = { lat: -25.344, lng: 131.031 };
+  const { Map } = await google.maps.importLibrary("maps");
+  const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
+
+  map = new Map(document.getElementById("map"), {
+    zoom: 4,
+    center: position,
+    mapId: "DEMO_MAP_ID",
+  });
+  const marker = new AdvancedMarkerElement({
+    map: map,
+    position: position,
+    title: "OBI",
+  });
+}
+
+initMap();
+
+
+
+
+
+
+
+
 let contactForm = document.getElementById("formcontact");
 let errorMsg = document.querySelectorAll(".error-message");
 let popup = document.getElementById("popupbox");
